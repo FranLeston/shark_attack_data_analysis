b0c6edaceb77f41a295cf261f5ebcf7135302cc8
Fran Leston <lestonramos@gmail.com>
2021-04-03 15:19:39 +0200

    cleaned data, took a lot of notes

--- src/limpieza_texto.py
+++ src/limpieza_texto.py
@@ -1,2 +1,24 @@
-def funcioncita(x):
+import numpy as np
+
+
+def clean_shark_species(x):
+    print(x)
     return str(x)
+
+
+def clean_age(age):
+    try:
+        age = int(age)
+    except:
+        age = 0
+    if (age > 0 and age <= 100):
+        return age
+    else:
+        return np.nan
+
+
+def filter_countries_by_attack_number(x):
+    if len(x) > 100:
+        return True
+    else:
+        return False
