c8b5e3781b6308a9636030ca7203ec79be478a9d
Fran Leston <lestonramos@gmail.com>
2021-04-03 17:39:12 +0200

    messing around with chart styles

--- src/limpieza_texto.py
+++ src/limpieza_texto.py
@@ -1,9 +1,14 @@
 import numpy as np
+import re
 
 
 def clean_shark_species(x):
-    print(x)
-    return str(x)
+    rgx = r'[A-Za-z]'
+    species = re.findall(rgx, x)
+    if species:
+        return species
+    else:
+        return np.nan
 
 
 def clean_age(age):
@@ -22,3 +27,17 @@ def filter_countries_by_attack_number(x):
         return True
     else:
         return False
+
+
+def clean_fatal(x):
+    if x == 'N' or x == 'Y':
+        return x
+    else:
+        return np.nan
+
+
+def clean_sex(x):  # Maybe rename this function it sounds weird
+    if x == 'M' or x == 'F':
+        return x
+    else:
+        return np.nan
